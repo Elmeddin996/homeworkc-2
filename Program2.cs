22e9f4d18a1664ba82d320486613989467b8f52b
elmeddin mirzeyev <elmaddinshm@code.edu.az>
2022-12-07 02:16:39 +0400

    c#homework

---
+++ Program2.cs
@@ -0,0 +1,21 @@
+﻿
+
+Console.WriteLine("Eded Daxil Edin");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+if (Number > 0)
+{
+    if (Number%21==0)
+    {
+        Console.WriteLine("Daxil edilen eded 7 ve 3-e qalıqsız bolunur");
+    }
+   else
+    {
+        Console.WriteLine("Daxil edilen eded 7 ve 3-e qaliqli bolunur");
+    }
+}
+else
+{
+    Console.WriteLine("Natural eded daxil edin!!!");
+}
+  
